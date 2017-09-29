46fe1289fcc5dac5aa5c6bc75490d0dc2920eeab
Invitado <Invitado@Lab6_PC6>
2017-09-29 12:30:50 -0500

    Actualizado controllers

--- Menor3MVC/src/controllers/ControllerMenor.java
+++ Menor3MVC/src/controllers/ControllerMenor.java
@@ -4,11 +4,38 @@
  * and open the template in the editor.
  */
 package controllers;
+import models.ModelMenor;
+import views.ViewMenor;
 
-/**
- *
- * @author mark
- */
 public class ControllerMenor {
+    ModelMenor    model_menor;
+    ViewMenor     view_menor;
     
+    public ControllerMenor(ModelMenor model_menor,ViewMenor view_menor){
+        this.model_menor = model_menor;
+        this.view_menor = view_menor;
+        view_menor.jbtn_menor.addActionListener(e -> jbtn_menor_click());
+     
+        initView();
+    }//constructor
+    
+    public void initView(){
+    view_menor.jtf_n1.setText(String.valueOf(model_menor.getN1()));
+    view_menor.jtf_n2.setText(String.valueOf(model_menor.getN2()));
+    view_menor.jtf_n3.setText(String.valueOf(model_menor.getN3()));
+    view_menor.jtf_m.setText(String.valueOf(model_menor.getM()));
+
+    
+    view_menor.setVisible(true);
+    
+    }
+    public void jbtn_mayor_click(){
+    model_menor.setN1(Integer.parseInt(view_menor.jtf_n1.getText()));
+    model_menor.setN2(Integer.parseInt(view_menor.jtf_n2.getText()));
+    model_menor.setN3(Integer.parseInt(view_menor.jtf_n3.getText()));
+    
+    model_menor.menor();
+    view_menor.jtf_m.setText(String.valueOf(model_menor.getM()));
+
+    }
 }
--- Menor3MVC/src/main/Main.java
+++ Menor3MVC/src/main/Main.java
@@ -4,11 +4,19 @@
  * and open the template in the editor.
  */
 package main;
+import models.ModelMenor;
+import views.ViewMenor;
+import controllers.ControllerMenor;
 
 /**
  *
  * @author mark
  */
 public class Main {
+    public static void main(String mar[]){
+        ModelMenor model_menor = new ModelMenor();
+        ViewMenor view_menor = new ViewMenor();
+        ControllerMenor controller_menor = new ControllerMenor(model_menor, view_menor);
+    }//main
     
 }
